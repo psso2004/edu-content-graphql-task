f60b79630310c64bda818f5aa12eb24da2e4a892
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-10-10 00:21:07 +0900

    feat: GraphQL 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin: string, pass) => {
+      // 개발 환경이 아닐경우 모든 출처에 대한 cors 허용하지 않도록 수정필요.
+      if (!origin || origin === 'https://studio.apollographql.com') {
+        return void pass(null, true);
+      } else {
+        return void pass(new BadRequestException('Not allowed by CORS'), false);
+      }
+    },
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
