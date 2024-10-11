e1cd84603266f16053af2ed75162ea4827f50465
psso2004 <pung-gigwon@pung-gigwon-ui-MacBookAir.local>
2024-10-11 11:19:03 +0900

    chore: typeorm migration 설정

---
+++ data-source-migration.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+const migrationsDir = process.env.MIGRATIONS_DIR || './migrations/*{.ts,.js}';
+
+export default new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'pung',
+  password: 'pungPassword!23',
+  database: 'codestates',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [migrationsDir],
+  migrationsTableName: 'migrations',
+  namingStrategy: new SnakeNamingStrategy(),
+});
